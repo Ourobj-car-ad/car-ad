809531799440edfa1c2aaca9df03907e80cb7ddb
JimZSR <471421496@qq.com>
2016-04-18 10:25:51 +0800

    添加了注册功能和界面

--- app/src/main/java/com/example/zhengsuren/olddriver/MainActivity.java
+++ app/src/main/java/com/example/zhengsuren/olddriver/MainActivity.java
@@ -6,11 +6,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button bt1;
+    private TextView regist;
     private Context mContext;
+    private EditText username;
+    private EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +23,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         bt1 = (Button) findViewById(R.id.button);
+        regist = (TextView) findViewById(R.id.regist);
         mContext = this;
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
 
         bt1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String url = "http://139.129.132.60/api/login";
+                new HttpThread(url,username.getText().toString(),password.getText().toString()).start();
                 Intent intent = new Intent(mContext,LoginSuccess.class);
+                startActivity(intent);
+            }
+        });
+
+        regist.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mContext,RegistPage.class);
                 startActivity(intent);
             }
         });
